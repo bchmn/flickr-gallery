1102c72eb103a60458377aad65e0ab36f66e5762
SHUANESH <shuanesh1817@gmail.com>
2021-11-22 19:44:24 +0200

    first-commit

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,54 +1,112 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import Image from '../Image';
-import './Gallery.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Image from "../Image";
+import "./Gallery.scss";
+import LightBoxEx from "../LightBox/LightBox";
+import InfiniteScroll from "react-infinite-scroll-component";
+// import { DragDropContext, Droppable } from "react-beautiful-dnd";
+// import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 
 class Gallery extends React.Component {
   static propTypes = {
-    tag: PropTypes.string
+    tag: PropTypes.string,
   };
 
   constructor(props) {
     super(props);
+    this.getImages = this.getImages.bind(this, this.props.tag);
+
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      galleryWidth: this.getGalleryWidth(),
+      isOpen: false,
+      currentImgForLightBox: {},
+      numberOfPhotos: 0,
+      currentTag: this.props.tag,
+      dragId:"",
     };
   }
 
-  getGalleryWidth(){
+  getGalleryWidth() {
     try {
-      return document.body.clientWidth;
+      return window.innerWidth;
     } catch (e) {
       return 1000;
     }
   }
-  getImages(tag) {
-    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
-    const baseUrl = 'https://api.flickr.com/';
+  getImages(tag2) {
+    const {isTagChanged, handleIsTagChanged, tag} = this.props
+    // console.log(isTagChanged);
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=${100}&format=json&nojsoncallback=${1}`;
+    const baseUrl = "https://api.flickr.com/";
     axios({
       url: getImagesUrl,
       baseURL: baseUrl,
-      method: 'GET'
+      method: "GET",
     })
-      .then(res => res.data)
-      .then(res => {
+      .then((res) => res.data)
+      .then((res) => {
         if (
           res &&
           res.photos &&
           res.photos.photo &&
-          res.photos.photo.length > 0
+          res.photos.photo.length > 0 
         ) {
-          this.setState({images: res.photos.photo});
-        }
+          const MySet = new Set();
+          
+          if (!isTagChanged) {
+            const newImages = [...this.state.images, ...res.photos.photo].map((img, index, arr)=>{
+              MySet.add(img)
+          });
+          // console.log(this.state.images.length);
+          this.setState({ images: Array.from(MySet)});
+          // this.setState({currentTag: tag});
+
+
+          }else {
+          this.setState({ images: res.photos.photo});
+          // this.setState({currentTag: tag});
+          handleIsTagChanged(); // false
+          
+          }
+
+        } 
       });
   }
 
+  handleDrag = (ev) => {
+    this.setState({dragId:ev.currentTarget.id});
+  };
+
+ handleDrop = (ev) => {
+    const {images, dragId} = this.state;
+    // console.log(dragId);
+    const dragImg = images.find((img) => img.id === dragId);
+    const dropImg = images.find((img) => img.id === ev.currentTarget.id);
+// console.log(dragImg);
+    const dragImgOrder = dragImg.id;
+    const dropImgOrder = dropImg.id;
+
+    const newImgState = images.map((img) => {
+      if (img.id === dragId) {
+        img.id = dropImgOrder;
+      }
+      if (img.id === ev.currentTarget.id) {
+        img.id = dragImgOrder;
+      }
+      console.log(img);
+      return img;
+    });
+    console.log("handleDrop");
+    this.setState({ images: newImgState});
+  };
+
+  //useEffect
   componentDidMount() {
     this.getImages(this.props.tag);
     this.setState({
-      galleryWidth: document.body.clientWidth
+      galleryWidth: window.innerWidth,
     });
   }
 
@@ -56,12 +114,86 @@ class Gallery extends React.Component {
     this.getImages(props.tag);
   }
 
+  handleDeleteImg(imgToDelete) {
+    const arrAfterDeleteImg = this.state.images.filter(
+      (img) => img.id !== imgToDelete.id
+    );
+    // console.log(arrAfterDeleteImg);
+    this.setState({ images: arrAfterDeleteImg });
+  }
+
+  handleIsOpenImg(currentImg) {
+    // console.log(currentImg);
+    this.setState({ currentImgForLightBox: currentImg });
+    this.setState({ isOpen: true });
+    // console.log(this.state.currentImgForLightBox);
+  }
+  hideLightBox() {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
+  handleScroll() {}
+
   render() {
+    const uniqId = new Date();
+    const random = Math.floor(Math.random(uniqId.setMilliseconds() * 10));
+    const {isTagChanged, handleIsTagChanged, tag} = this.props
+    const {isOpen, currentImgForLightBox ,images ,galleryWidth} = this.state;
+
     return (
-      <div className="gallery-root">
-        {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
-        })}
+      <div className={isOpen ?"gallery-root":"active"}>
+        {isOpen ? (
+          <div
+            className="gallery-lightBox"
+            onClick={this.hideLightBox.bind(this)}
+          >
+            <LightBoxEx
+              images={images}
+              currentImgForLightBox={currentImgForLightBox}
+              isOpen={isOpen}
+            />
+          </div>
+        ) : (
+          <InfiniteScroll
+            dataLength={images.length}
+            next={this.getImages.bind(tag)}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            scrollThreshold={0.3}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            {/* <DragDropContext
+              onDragEnd={(...props) => {
+                console.log(props);
+              }}
+            > */}
+              <div className={isOpen ?"gallery-root":"active"}>
+              {/* <Droppable> */}
+              {images.sort((a,b)=>a.id - b.id).map((dto, index) => {
+                return (
+                  <Image
+                    handleIsOpenImg={this.handleIsOpenImg.bind(this, dto)}
+                    key={"image-"+ uniqId.getFullYear().toString() + index + random + dto.id }
+                    dto={dto}
+                    handleDeleteImg={this.handleDeleteImg.bind(this, dto)}
+                    images={images}
+                    galleryWidth={galleryWidth}
+                    handleDrag={this.handleDrag.bind(this)}
+                    handleDrop={this.handleDrop.bind(this)}
+                  />
+                );
+              })}
+              {/* </Droppable> */}
+              </div>
+            {/* </DragDropContext> */}
+          </InfiniteScroll>
+        )}
       </div>
     );
   }
---
+++ src/components/LightBox/LightBox.js
@@ -0,0 +1,74 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./lightBox.scss";
+import FontAwesome from "react-fontawesome";
+
+
+class LightBoxEx extends React.Component {
+  static propTypes = {
+    currentImgForLightBox: PropTypes.object,
+  };
+  constructor(props) {
+    super(props);
+    this.showNext = this.showNext.bind(this);
+    this.showPrev = this.showPrev.bind(this);
+    this.state = {
+      photoIndex: 0,
+      isOpen: this.props.isOpen,
+      lightBoxMood: this.props.currentImgForLightBox,
+      imageToShow: `https://farm${this.props.currentImgForLightBox.farm}.staticflickr.com/${this.props.currentImgForLightBox.server}/${this.props.currentImgForLightBox.id}_${this.props.currentImgForLightBox.secret}.jpg`,
+    };
+  }
+
+  showNext = (e) => {
+    e.stopPropagation();
+    const { images } = this.props;
+    let currentIndex = images.indexOf(this.state.lightBoxMood);
+    console.log(currentIndex);
+    if (currentIndex >= images.length - 1) {
+      this.setState({
+        isOpen: false,
+      });
+    } else {
+      let nextImage = images[currentIndex + 1];
+      console.log(nextImage);
+      this.setState({
+        imageToShow: `https://farm${nextImage.farm}.staticflickr.com/${nextImage.server}/${nextImage.id}_${nextImage.secret}.jpg`,
+        lightBoxMood: nextImage,
+      });
+    }
+    // window.location.href = `https://farm${this.props.currentImgForLightBox.farm}.staticflickr.com/${this.props.currentImgForLightBox.server}/${this.props.currentImgForLightBox.id}_${this.props.currentImgForLightBox.secret}.jpg`
+  };
+
+  showPrev = (e) => {
+    e.stopPropagation();
+    const { images } = this.props;
+    let currentIndex = images.indexOf(this.state.lightBoxMood);
+    console.log(currentIndex);
+    if (currentIndex >= images.length - 1) {
+      this.setState({
+        isOpen: false,
+      });
+    } else {
+      let prevImage = images[currentIndex - 1];
+      console.log(prevImage);
+      this.setState({
+        imageToShow: `https://farm${prevImage.farm}.staticflickr.com/${prevImage.server}/${prevImage.id}_${prevImage.secret}.jpg`,
+        lightBoxMood: prevImage,
+      });
+    }
+    // window.location.href = `https://farm${this.props.currentImgForLightBox.farm}.staticflickr.com/${this.props.currentImgForLightBox.server}/${this.props.currentImgForLightBox.id}_${this.props.currentImgForLightBox.secret}.jpg`
+  };
+
+  render() {
+    return (
+      <div className="continer-lightBox">
+        <button className="lightBox-button" onClick={this.showPrev}><i className="fas fa-chevron-circle-left"></i></button>
+        <img className="lightBox-img" src={`${this.state.imageToShow}`} />
+        <button className="lightBox-button" onClick={this.showNext}><i className="fas fa-chevron-circle-right"></i></button>
+      </div>
+    );
+  }
+}
+
+export default LightBoxEx;
