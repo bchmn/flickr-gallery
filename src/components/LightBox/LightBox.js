a388a6742c85275759c7df3d55613a7e4d7f58d0
SHUANESH <shuanesh1817@gmail.com>
2021-11-25 14:10:47 +0200

    delete-console

--- src/components/LightBox/LightBox.js
+++ src/components/LightBox/LightBox.js
@@ -23,20 +23,17 @@ class LightBoxEx extends React.Component {
     e.stopPropagation();
     const { images } = this.props;
     let currentIndex = images.indexOf(this.state.lightBoxMood);
-    console.log(currentIndex);
     if (currentIndex >= images.length - 1) {
       this.setState({
         isOpen: false,
       });
     } else {
       let nextImage = images[currentIndex + 1];
-      console.log(nextImage);
       this.setState({
         imageToShow: `https://farm${nextImage.farm}.staticflickr.com/${nextImage.server}/${nextImage.id}_${nextImage.secret}.jpg`,
         lightBoxMood: nextImage,
       });
     }
-    // window.location.href = `https://farm${this.props.currentImgForLightBox.farm}.staticflickr.com/${this.props.currentImgForLightBox.server}/${this.props.currentImgForLightBox.id}_${this.props.currentImgForLightBox.secret}.jpg`
   };
 
   showPrev = (e) => {
